ec1903460497df6b05b9f02976504cf79ff5a706
Nitin Passa <nitinkpassa@gmail.com>
2014-05-05 05:40:32 -0400

    Added empty hypergraph visit functions

---
+++ hypergraph/visits.py
@@ -0,0 +1,13 @@
+from .hypergraph import Hypergraph
+
+def visit(HyperGraph):
+    '''
+    A simple visit of the nodes of the hypergraph
+    '''
+    pass
+
+def bVisit(HyperGraph):
+    '''
+    A B-Visit of the nodes of the hypergraoh
+    '''
+    pass
